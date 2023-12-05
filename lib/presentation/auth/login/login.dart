8e3847639bfa998a7810fbb7c9d69099d986f9c9
unknown <Administrator@ALOT-PC04.alotgroup.local>
2023-12-05 12:01:43 +0400

    added validations to login screen using form widget

--- lib/presentation/auth/login/login.dart
+++ lib/presentation/auth/login/login.dart
@@ -230,10 +230,17 @@ class desktopWidget extends StatelessWidget {
   }
 }
 
-class mobileWidget extends StatelessWidget {
-  const mobileWidget({
+class mobileWidget extends StatefulWidget {
+  mobileWidget({
     super.key,
   });
+
+  @override
+  State<mobileWidget> createState() => _mobileWidgetState();
+}
+
+class _mobileWidgetState extends State<mobileWidget> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -242,194 +249,239 @@ class mobileWidget extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.only(top: 10, left: 16, right: 16, bottom: 10),
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Text(AppLocalizations.of(context)!.hello.toString()),
-              TextWidget(
-                title: "Welcome back!",
-                textStyle: TextStyle(
-                    color: colorBlue,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500),
-              ),
-              height5,
-              TextWidget(
-                title: "Member Login",
-                textStyle: TextStyle(
-                    color: colorBlack,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold),
-              ),
-              height20,
-              TextWidget(
-                  title: "Email Address",
+      child: Form(
+        key: _formKey,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Text(AppLocalizations.of(context)!.hello.toString()),
+                TextWidget(
+                  title: "Welcome back!",
                   textStyle: TextStyle(
-                      color: colorBlack, fontWeight: FontWeight.normal)),
-              height10,
-              TextFormField(
-                controller: userNameController,
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                    constraints: BoxConstraints.expand(height: 50),
-                    label: TextWidget(
-                        title: "Default username kminchelle",
-                        textStyle: TextStyle()),
-                    labelStyle: TextStyle(
-                        color: colorGrey, fontWeight: FontWeight.w500),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: colorGrey)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: colorGrey)),
-                    floatingLabelBehavior: FloatingLabelBehavior.never),
-                maxLines: 1,
-              ),
-              height30,
-              TextWidget(
-                  title: "Password",
+                      color: colorBlue,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500),
+                ),
+                height5,
+                TextWidget(
+                  title: "Member Login",
                   textStyle: TextStyle(
-                      color: colorBlack, fontWeight: FontWeight.normal)),
-              height10,
-              TextFormField(
-                controller: passwordController,
-                textInputAction: TextInputAction.done,
-                obscureText: true,
-                decoration: InputDecoration(
-                    constraints: BoxConstraints.expand(height: 50),
-                    label: TextWidget(
-                        title: "Default password 0lelplR",
-                        textStyle: TextStyle()),
-                    labelStyle: TextStyle(
-                        color: colorGrey, fontWeight: FontWeight.w500),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: colorGrey)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: colorGrey)),
-                    floatingLabelBehavior: FloatingLabelBehavior.never),
-                maxLines: 1,
-              ),
-              height30,
-              SizedBox(
-                width: double.infinity,
-                height: 45,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    final result = await Repository.instance.login(
-                        userNameController.text, passwordController.text);
+                      color: colorBlack,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                height20,
+                TextWidget(
+                    title: "Email Address",
+                    textStyle: TextStyle(
+                        color: colorBlack, fontWeight: FontWeight.normal)),
+                height10,
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter username';
+                    }
+                    return null;
+                  },
+                  controller: userNameController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 10),
+                      // constraints: BoxConstraints.expand(height: 50),
+                      constraints:
+                          const BoxConstraints(maxHeight: 70, minHeight: 35),
+                      label: TextWidget(
+                          title: "Default username kminchelle",
+                          textStyle: TextStyle()),
+                      labelStyle: TextStyle(
+                          color: colorGrey, fontWeight: FontWeight.w500),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorGrey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorGrey)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorRed)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorRed)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorGrey)),
+                      floatingLabelBehavior: FloatingLabelBehavior.never),
+                  maxLines: 1,
+                ),
+                height30,
+                TextWidget(
+                    title: "Password",
+                    textStyle: TextStyle(
+                        color: colorBlack, fontWeight: FontWeight.normal)),
+                height10,
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter password';
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  textInputAction: TextInputAction.done,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 10),
+                      // constraints: BoxConstraints.expand(height: 50),
+                      constraints:
+                          const BoxConstraints(maxHeight: 70, minHeight: 35),
+                      label: TextWidget(
+                          title: "Default password 0lelplR",
+                          textStyle: TextStyle()),
+                      labelStyle: TextStyle(
+                          color: colorGrey, fontWeight: FontWeight.w500),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorGrey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorGrey)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorRed)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorRed)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: colorGrey)),
+                      floatingLabelBehavior: FloatingLabelBehavior.never),
+                  maxLines: 1,
+                ),
+                height30,
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final result = await Repository.instance.login(
+                            userNameController.text, passwordController.text);
 
-                    result.fold((l) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(l.message),
-                      ));
-                    }, (r) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("successfully logged in"),
-                      ));
+                        result.fold((l) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(l.message),
+                          ));
+                        }, (r) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("successfully logged in"),
+                          ));
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return DashboardScreen();
+                          }));
+                        });
+                      }
+                    },
+                    child: TextWidget(
+                        title: "Submit",
+                        textStyle: TextStyle(fontWeight: FontWeight.bold)),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(colorBlue),
+                    ),
+                  ),
+                ),
+                height40,
+                TextWidget(
+                    title: "Forgot Password?",
+                    textStyle: TextStyle(
+                        color: colorBlue, fontWeight: FontWeight.normal)),
+                height20,
+                Row(children: [
+                  Expanded(child: Divider()),
+                  width30,
+                  TextWidget(
+                    title: "or",
+                    textStyle: TextStyle(),
+                  ),
+                  width30,
+                  Expanded(child: Divider()),
+                ]),
+                height40,
+                SizedBox(
+                  width: double.infinity,
+                  height: 40,
+                  child: ElevatedButton.icon(
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      final data = await signInWithGoogle();
+                      print(data);
+                    },
+                    icon: Image.asset(
+                      'assets/images/google.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                    label: TextWidget(
+                        title: "Signup with Google", textStyle: TextStyle()),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(colorRed)),
+                  ),
+                ),
+                height10,
+                SizedBox(
+                  width: double.infinity,
+                  height: 40,
+                  child: ElevatedButton.icon(
+                    onPressed: () async {
+                      await signInWithFacebook();
+                    },
+                    icon: Image.asset(
+                      'assets/images/facebook.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                    label: TextWidget(
+                        title: "Connect with Facebook", textStyle: TextStyle()),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(colorBlueAccent)),
+                  ),
+                ),
+                height25,
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  TextWidget(
+                    title: "Don't have an Account?",
+                    textStyle: TextStyle(
+                      color: colorGrey,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  width5,
+                  InkWell(
+                    onTap: () {
+                      FocusManager.instance.primaryFocus?.unfocus();
                       Navigator.of(context)
                           .push(MaterialPageRoute(builder: (context) {
-                        return DashboardScreen();
+                        return RegisterScreen();
                       }));
-                    });
-                  },
-                  child: TextWidget(
-                      title: "Submit",
-                      textStyle: TextStyle(fontWeight: FontWeight.bold)),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(colorBlue),
+                    },
+                    child: TextWidget(
+                      title: "Sign up",
+                      textStyle: TextStyle(
+                          color: colorBlue, fontWeight: FontWeight.normal),
+                    ),
                   ),
-                ),
-              ),
-              height40,
-              TextWidget(
-                  title: "Forgot Password?",
-                  textStyle: TextStyle(
-                      color: colorBlue, fontWeight: FontWeight.normal)),
-              height20,
-              Row(children: [
-                Expanded(child: Divider()),
-                width30,
-                TextWidget(
-                  title: "or",
-                  textStyle: TextStyle(),
-                ),
-                width30,
-                Expanded(child: Divider()),
-              ]),
-              height40,
-              SizedBox(
-                width: double.infinity,
-                height: 40,
-                child: ElevatedButton.icon(
-                  onPressed: () async {
-                    await FirebaseAuth.instance.signOut();
-                    final data = await signInWithGoogle();
-                    print(data);
-                  },
-                  icon: Image.asset(
-                    'assets/images/google.png',
-                    width: 20,
-                    height: 20,
-                  ),
-                  label: TextWidget(
-                      title: "Signup with Google", textStyle: TextStyle()),
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(colorRed)),
-                ),
-              ),
-              height10,
-              SizedBox(
-                width: double.infinity,
-                height: 40,
-                child: ElevatedButton.icon(
-                  onPressed: () async {
-                    await signInWithFacebook();
-                  },
-                  icon: Image.asset(
-                    'assets/images/facebook.png',
-                    width: 20,
-                    height: 20,
-                  ),
-                  label: TextWidget(
-                      title: "Connect with Facebook", textStyle: TextStyle()),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll(colorBlueAccent)),
-                ),
-              ),
-              height25,
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                TextWidget(
-                  title: "Don't have an Account?",
-                  textStyle: TextStyle(
-                    color: colorGrey,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                width5,
-                InkWell(
-                  onTap: () {
-                    FocusManager.instance.primaryFocus?.unfocus();
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return RegisterScreen();
-                    }));
-                  },
-                  child: TextWidget(
-                    title: "Sign up",
-                    textStyle: TextStyle(
-                        color: colorBlue, fontWeight: FontWeight.normal),
-                  ),
-                ),
-              ]),
-            ],
+                ]),
+              ],
+            ),
           ),
         ),
       ),
